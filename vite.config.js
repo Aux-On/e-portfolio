fbf75d67a2f5d65fac2bb8e470b2717496a8b1f5
Jaffar3984 <sjraza2019@gmail.com>
2025-04-21 14:41:19 -0400

    2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace `your-username` and `repo-name` below!
 export default defineConfig({
+  base: '/my-portfolio/', // <- very important!
   plugins: [react()],
-})
+});
